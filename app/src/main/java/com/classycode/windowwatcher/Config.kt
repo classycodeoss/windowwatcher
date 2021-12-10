96ba673d65d742c3f5a02681afb98f0d6343857c
Alex Suzuki <alex@classycode.com>
2021-12-10 14:21:23 +0100

    Cleaned up Config

--- app/src/main/java/com/classycode/windowwatcher/Config.kt
+++ app/src/main/java/com/classycode/windowwatcher/Config.kt
@@ -6,17 +6,16 @@ class Config {
 
         // these values are obtained from the MQTT integration in the Things Network Console
         const val brokerUrl = "tcp://eu1.cloud.thethings.network:1883"
-        const val brokerUsername = "<your-app-id>e@ttn"
+        const val brokerUsername = "<your-app-id>@ttn"
         const val brokerPassword = "<your api key>"
 
-        const val clientId = "<your client id" // doesn't matter
+        const val clientId = "<your client id>" // doesn't matter
 
-        // the EMS sensors
+        // A list of end device IDs
         val devices = listOf(
-            // a list of device ids
-            "your-device-id-1",
-            "your-device-id-2"
-            )
+            "my-device-id-1",
+            "my-device-id-2"
+        )
 
         // the uplink topics
         val topics: List<String> =
